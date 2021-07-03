3fc0934427c53daa2adc1997c1fa80d2c4b5d865
durantegeorge1 <durantegeorge@hotmail.com>
2021-07-02 21:55:52 -0400

    Add files via upload

---
+++ LeadApp.API/APIServiceCollection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using LeadApp.Domain;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LeadApp.API
+{
+    public static class APIServiceCollection
+    {
+        public static void AddAPIServiceCollection(this IServiceCollection services)
+        {
+            DomainServiceCollection.AddDomainServiceCollection(services);
+        }
+    }
+}
